42feb9e47adce8f3702827a0a9bd6a17450d4c55
MikeJohnshoy <mike.johnshoy@gmail.com>
2025-03-12 16:58:12 -0400

    Update sdr.h
    - add new CW paddle modes (SIDESWIPE and ULTIMATIC)
    - add low-level symbols (CW_SQUEEZE and CW_SQUEEZEOFF) for new keyer state machine

--- src/sdr.h
+++ src/sdr.h
@@ -228,13 +228,16 @@ time_t time_sbitx();
 #define CW_DASH_DELAY (8) 
 #define CW_WORD_DELAY (16) 
 #define CW_DOWN (32) 
+#define CW_SQUEEZE (64)
+#define CW_SQUEEZEOFF (128)
 
 //straight key, iambic, keyboard
 #define CW_STRAIGHT 0
 #define CW_IAMBIC	1
 #define CW_IAMBICB 2	
 #define CW_KBD 3
-
+#define CW_SIDESWIPE 4
+#define CW_ULTIMATIC 5
 
 int key_poll();
 int key_poll2();
