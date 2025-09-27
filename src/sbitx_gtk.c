91cde27e14802b91fcefc4547b72c1c08b6e9dc4
MikeJohnshoy <mike.johnshoy@gmail.com>
2025-09-27 09:48:32 -0400

    Adjust moving average and tuning rate values
    I tried different tuning acceleration 'profiles'.
    I decided I wanted to leave the rate at the UI selected tuning rate for slow to moderate tuning rates, and then start to accelerate.  Once you start tweaking the profile it's hard to stop ...

--- src/sbitx_gtk.c
+++ src/sbitx_gtk.c
@@ -4670,7 +4670,7 @@ int do_tuning(struct field *f, cairo_t *gfx, int event, int a, int b, int c) {
     // measure time between tuning steps and keep weighted moving average
     static uint64_t last_us = 0;
     static double ema_rate = 0.0;  // events per second, smoothed
-    const double alpha = 0.20;     // moving average factor; higher = more responsive
+    const double alpha = 0.50;     // moving average factor; higher = more responsive
 
     int base_step = tuning_step;  // keep user step chosen in UI is immutable per event
     int local_step = base_step;   // computed each event
@@ -4688,16 +4688,16 @@ int do_tuning(struct field *f, cairo_t *gfx, int event, int a, int b, int c) {
           // compute weighted moving average
           ema_rate = (alpha * inst_rate) + (1.0 - alpha) * ema_rate;
 
-          // set tuning rate multiplier based on average rate
+          // set tuning rate multiplier based on weighted moving average rate
           int mult;
-          if (ema_rate < 10.0)
+          if (ema_rate < 15.0)
             mult = 1;    // 10 Hz steps stay at 10 Hz with slow turns
-          else if (ema_rate < 20.0)
-            mult = 5;    // 10 Hz steps become 50 Hz steps
-          else if (ema_rate < 40.0)
+          else if (ema_rate < 30.0)
             mult = 10;    // 10 Hz steps become 100 Hz steps
-          else   // ema_rate > 40.0
+          else if (ema_rate < 45.0)
             mult = 50;    // 10 Hz steps become 500 Hz steps
+          else   // ema_rate > 45.0
+            mult = 100;   // 10 Hz steps become 1000 Hz steps
          
           // set a max tuning rate
           // consider user selecting 10Khz tuning step size we wouldn't want to exceed 50K steps
